2949fb2208426d34180b75833b227203da084655
Michael Staake <michael@staake.ai>
2026-07-01 14:48:29 -0700

    2026 theme, update version info

--- theme/2026/index.php
+++ theme/2026/index.php
@@ -2,8 +2,8 @@
 
 return [
     'name'            => '2026',
-    'description'     => 'A clean, responsive off-black and off-white starter theme.',
-    'version'     => '2026.06.30.001',
+    'description'     => 'A clean, responsive starter theme.',
+    'version'     => '2026.07.01.001',
     'author'      => 'Michael Staake',
     'menu_locations'  => [
         'primary' => 'Primary Navigation',
